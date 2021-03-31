ca8f9dd6d35e0cb23d02ca172d2d38a2a3d8fd65
Rikio Campbell <rikiocamp@gmail.com>
2021-03-30 20:50:58 -0700

    added meal model. added index&meal seed.

---
+++ models/Meal.js
@@ -0,0 +1,73 @@
+// import important parts of sequelize library
+const { Model, DataTypes } = require('sequelize');
+// import our database connection from config.js
+const sequelize = require('../config/connection');
+
+// Initialize Meal model (table) by extending off Sequelize's Model class
+class Meal extends Model {}
+
+// set up fields and rules for Meal model
+Meal.init(
+  {
+    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    meal_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    calories: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true
+      }
+    },
+    fat: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 10,
+        validate: {
+          isNumeric: true
+        }
+      },
+    carbs: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 10,
+        validate: {
+            isNumeric: true
+            }
+    },
+    protein: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 10,
+        validate: {
+            isNumeric: true
+            }
+        },
+    sodium: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 10,
+        validate: {
+            isNumeric: true
+            }
+        },         
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'meal',
+  }
+);
+
+module.exports = Meal;
