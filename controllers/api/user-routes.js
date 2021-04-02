231e109267777fc5ff3802bba623103fcb45fa20
Danielle Orley <danielle.m.orley1993@gmail.com>
2021-04-01 17:58:22 -0700

    working on the api folder now, I have some of the js files mostly finished just have to fix a few things after stuff comes together

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,153 @@
+const router = require('express').Router();
+//model, when all files are complete
+const { User, Blog, Meal } = require('../../models');
+
+// GET all users
+router.get('/', (req, res) => {
+    User.findAll({
+        attributes: { exclude: ['password'] }
+    })
+        .then(userData => res.json(userData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+// GET a single user by ID
+router.get('/:id', (req, res) => {
+    User.findOne({
+        attributes: {
+            exclude: ['password']
+        },
+        where: {
+            id: req.params.id
+        },
+        include: [
+            {
+                model: Blog,
+                attributes: ['id', 'title', 'content']
+            },
+            {
+                model: Comment,
+                attributes: ['id', 'content'],
+            }
+        ]
+    })
+        .then(userData => {
+            if (!userData) {
+                res.status(404).json({ message: 'No developer found with that ID!' });
+                return;
+            }
+            res.json(userData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+// POST - Create a user
+router.post('/', (req, res) => {
+    User.create({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password
+    })
+        .then(userData => {
+            req.session.save(() => {
+                req.session.user_id = userData.id;
+                req.session.username = userData.username;
+                req.session.loggedIn = true;
+
+                res.json(userData);
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+// User login
+router.post('/sign-in', (req, res) => {
+    User.findOne({
+        where: {
+            username: req.body.username
+        }
+    })
+        .then(userData => {
+            if (!userData) {
+                res.json(400).json({ message: "That username doesn't exist!" });
+                return;
+            };
+
+            const validPassword = userData.passwordConfirm(req.body.password);
+
+            if (!validPassword) {
+                res.status(400).json({ message: "That's not the right password..." });
+                return;
+            }
+
+            req.session.save(() => {
+                req.session.user_id = userData.id;
+                req.session.username = userData.username;
+                req.session.loggedIn = true;
+
+                res.json({ user: userData, message: `Hi ${userData.username}, you're logged in! Time to blog!!` });
+            });
+        });
+});
+
+// User logout
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).end();
+    }
+});
+
+// PUT - update a user
+router.put('/:id', (req, res) => {
+    User.update(req.body, {
+        individualHooks: true,
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(userData => {
+            if (!userData) {
+                res.status(400).json({ message: 'No developer found with that ID!' });
+            }
+            res.json(userData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+// DELETE - delete a user
+router.delete('/:id', (req, res) => {
+    User.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(userData => {
+            if (!userData) {
+                res.status(404).json({ message: 'No developer found with that ID!' });
+                return;
+            }
+            res.json(userData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+module.exports = router;
