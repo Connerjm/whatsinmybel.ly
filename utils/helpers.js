2eef5b99683aad5984738b39e67a04f5880dbf6a
Danielle Orley <danielle.m.orley1993@gmail.com>
2021-04-03 10:49:07 -0700

    update utils files

---
+++ utils/helpers.js
@@ -0,0 +1,6 @@
+module.exports = {
+    timeStamp: date => {
+        // return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(date).getFullYear()}`;
+        return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(date).getFullYear()} at ${new Date(date).getHours()}:${new Date(date).getMinutes()}`;
+    }
+  }
