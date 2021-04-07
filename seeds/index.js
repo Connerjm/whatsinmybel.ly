a88b396ec05e49e55055ba7a834145f67f699bdd
Rikio Campbell <rikiocamp@gmail.com>
2021-04-06 21:33:51 -0700

    updating user-routes, and adding faker

--- seeds/index.js
+++ seeds/index.js
@@ -1,4 +1,6 @@
 //Imports.
+var faker = require('faker');
+const fs = require('fs');
 const sequelize = require('../config/connection');
 const { User } = require('../models');
 const { Meal } = require('../models');
@@ -6,6 +8,60 @@ const { Meal } = require('../models');
 //Grab the json data files.
 const userData = require('./userData.json');
 const mealData = require('./mealData.json');
+
+const fakerRun = () => {
+  var randomName = faker.name.findName();
+  var randomEmail = faker.internet.email();
+  var randomPassword = faker.internet.password();
+  return [randomName,randomEmail,randomPassword]
+}
+
+const json1 = fakerRun();
+const json2 = fakerRun();
+const json3 = fakerRun();
+const json4 = fakerRun();
+const json5 = fakerRun();
+
+const userJson = () => 
+    `[
+      {
+        "username": "${json1[0]}",
+        "email": "${json1[1]}",
+        "password": "${json1[2]}"
+      },
+      {
+        "username": "${json2[0]}",
+        "email": "${json2[1]}",
+        "password": "${json2[2]}"
+      },
+      {
+        "username": "${json3[0]}",
+        "email": "${json3[1]}",
+        "password": "${json3[2]}"
+      },
+      {
+        "username": "${json4[0]}",
+        "email": "${json4[1]}",
+        "password": "${json4[2]}"
+      },
+      {
+        "username": "${json5[0]}",
+        "email": "${json5[1]}",
+        "password": "${json5[2]}"
+      }
+    ]`
+
+const init = () => {
+  try {
+    const jsonfile = userJson();
+    fs.writeFileSync('./seeds/userData.json', jsonfile)
+    console.log('Successfully wrote to json');
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+init()
 
 //Single function to seed the DB.
 const seedDatabase = async () => {
