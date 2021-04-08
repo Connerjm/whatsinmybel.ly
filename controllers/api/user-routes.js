6ef0f996cce84f5cbaa0a311312966ba485d7340
Rikio Campbell <rikiocamp@gmail.com>
2021-04-07 17:01:28 -0700

    Update user-routes.js

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -36,7 +36,6 @@ router.get('/:id', (req, res) => {
                 return;
             }
             res.json(userData);
-            console.log(userData)
         })
         .catch(err => {
             console.log(err);
