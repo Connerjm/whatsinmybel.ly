a856bebd8912ed3a14057b9cb4979dda6ee8d904
Conner Martin <connerjm2@gmail.com>
2021-04-07 22:59:46 -0700

    more comments.

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,11 @@
+//Imports.
 const router = require('express').Router();
-
 const userRoutes = require('./user-routes');
 const mealRoutes = require('./meal-routes');
 
-//js routes
+//Define routes.
 router.use('/user', userRoutes);
 router.use('/meal', mealRoutes);
 
+//Exports
 module.exports = router;
